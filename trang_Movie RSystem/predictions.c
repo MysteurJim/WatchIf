99d9c8dced60c65eef2ad5331c7e26735fa947a8
Jane <94185643+Jane1702@users.noreply.github.com>
2023-04-21 01:00:37 +0200

    Update predictions.c

--- trang_Movie RSystem/predictions.c
+++ trang_Movie RSystem/predictions.c
@@ -5,17 +5,20 @@
 int make_prediction(double *user, int *similar_users, int no_of_susers, double *similarity, double *utility_matrix, int *recommended_movies, double *predicted_ratings, int No_of_movies){
 	int i=0,k=0;
 	int no_of_recommended_movies = 0;
-	for(i=0;i<No_of_movies;i++){ //traverse through each movie
+	for(i=0;i<No_of_movies;i++)
+	{ //traverse through each movie
 		double sum1=0, sum2=0;
 		int count=0;
 		if(user[i]==0){ //if not rated by the user
-			for(k=0;k<no_of_susers;k++){ //traverse through similar users
+			for(k=0;k<no_of_susers;k++)
+			{ //traverse through similar users
 				if(utility_matrix[similar_users[k]*No_of_movies + i]==0) continue; //all similar users who have also rated movie i
 				sum1 += similarity[similar_users[k]]*utility_matrix[similar_users[k]*No_of_movies + i];
 				sum2 += similarity[similar_users[k]];
 				count++;
 			}
-			if(count>1){ //the movie is common between atleast two users otherwise if it only has one user then we will get that same rating
+			if(count>1)
+			{ //the movie is common between atleast two users otherwise if it only has one user then we will get that same rating
 				recommended_movies[no_of_recommended_movies] = i; //add movie index to recommended movies
 				predicted_ratings[no_of_recommended_movies] = sum1/sum2; //make prediction
 				no_of_recommended_movies++;
