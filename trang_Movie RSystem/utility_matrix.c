d6d348ec45729c0726d0e451c71edcc4f01d22e3
Jane <94185643+Jane1702@users.noreply.github.com>
2023-04-20 23:58:18 +0200

    Update utility_matrix.c

--- trang_Movie RSystem/utility_matrix.c
+++ trang_Movie RSystem/utility_matrix.c
@@ -50,20 +50,22 @@ void get_utility_matrix(double *utility_matrix, char *s, int No_of_movies, int N
 	char tmp[1024];
 	int i=0, j=0, k=0;
 	FILE *fstream = fopen(s,"r");
-	while((line=fgets(tmp,sizeof(tmp),fstream))!=NULL){
+	while((line=fgets(tmp,sizeof(tmp),fstream))!=NULL)
+	{
 		record = strtok(line,",");
-		while(record!=NULL){
-			if(k==0){ //first string is user id, which will give our row
-                i = atoi(record)-1;
-			}else if(k==1){ //second string is movie id which will give our coloumn
+		while(record!=NULL)
+		{
+			if(k==0)
+			{ //first string is user id, which will give our row
+                		i = atoi(record)-1;
+			}
+			else if(k==1)
+			{ //second string is movie id which will give our coloumn
 				j = atoi(record)-1;
-			}else { //third is the actual rating
-			    /*if(l == uid){
-                    utility_matrix[i*No_of_movies+j] = 0.0;
-			    }*/
-			    //else{
-                    utility_matrix[i*No_of_movies + j] = atof(record); //converting string to float/double
-			    //}
+			}
+			else 
+			{ //third is the actual rating
+                    		utility_matrix[i*No_of_movies + j] = atof(record); //converting string to float/double
 			}
 			record = strtok(NULL,",");
 			k++;
