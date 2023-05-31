8b87d3756f2f10641f7f433cd8d58459063657cd
Jane <94185643+Jane1702@users.noreply.github.com>
2023-05-31 02:08:59 +0200

    Update utility_matrix.h

--- trang_Movie RSystem/utility_matrix.h
+++ trang_Movie RSystem/utility_matrix.h
@@ -6,7 +6,7 @@ char* get_movie_names(int index_movie);
 
 char* get_movie_genres(int index_movie);
 
-void get_utility_matrix();
+void get_utility_matrix(double **utility_matrixe);
 
 void new_user_movies(double *newuser,int uid);
 //defined as double *newuser = (double *)malloc(sizeof(double) * No_of_movies); where ratings of new user will be stored
