54d7aa1e98db92bb343fd33ee4869cb2972bc899
Jane <94185643+Jane1702@users.noreply.github.com>
2023-05-31 02:18:31 +0200

    Update predictions.h

--- trang_Movie RSystem/predictions.h
+++ trang_Movie RSystem/predictions.h
@@ -3,22 +3,15 @@
 #define PREDICTIONS_H
 
 int make_prediction(
-					double *user, //new  user ratings defiend as double *user = (double *)malloc(sizeof(double) * No_of_movies); 
-					
-					int *similar_users, //ids of all similar users based on clustering
-					
-					int no_of_susers, //number of similar users
-					
-					double *similarity, //similarity with all the existing user
-					
-					double *utility_matrix, //utility matrix
-					
-					int *recommended_movies, //list of recommended movies to be saved in
-					
-					double *predicted_ratings, //predicted ratigs of those movies to be saved in
-					
-					int No_of_movies
-					); //returns number of recommended movies
+			double *user, //new  user ratings defiend as double *user = (double *)malloc(sizeof(double) * No_of_movies); 	
+			int *similar_users, //ids of all similar users based on clustering			
+			int no_of_susers, //number of similar users					
+			double *similarity, //similarity with all the existing user
+			double *utility_matrix, //utility matrix
+			int *recommended_movies, //list of recommended movies to be saved in		
+			double *predicted_ratings, //predicted ratigs of those movies to be saved in	
+			int No_of_movies
+			); //returns number of recommended movies
 					
 void test_predictions(
 					  double *user, 
